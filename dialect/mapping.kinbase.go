96487650f2ff20fe4fe04fe79a70fa23df16018a
朱亮 <teamide@163.com>
2022-11-14 21:50:14 +0800

    Update mapping.kinbase.go

--- dialect/mapping.kinbase.go
+++ dialect/mapping.kinbase.go
@@ -1,15 +1,20 @@
 package dialect
 
 func NewMappingKinBase() (mapping *SqlMapping) {
+
+	// http://www.yaotu.net/biancheng/21946.html
+	// https://www.modb.pro/db/442114
+
 	mapping = NewMappingOracle()
 	mapping.dialectType = TypeKinBase
 
-	mapping.dialectType = TypeShenTong
 	mapping.OwnerCreate = `
 CREATE USER {ownerName} WITH PASSWORD {sqlValuePack(ownerPassword)};
+CREATE SCHEMA {ownerName} AUTHORIZATION {ownerName};
 `
 	mapping.OwnerDelete = `
-DROP USER {ownerName} cascade;
+DROP SCHEMA {ownerName} CASCADE;
+DROP OWNED BY {ownerName} CASCADE;
 `
 	mapping.OwnerNamePackChar = ""
 	return
