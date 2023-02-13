889c99015c554a4a4686ea6b54f8ebe5f964099e
朱亮 <teamide@163.com>
2023-02-13 18:21:09 +0800

    GBase SQL占位符使用？

--- dialect/mapping.gbase.go
+++ dialect/mapping.gbase.go
@@ -2,7 +2,6 @@ package dialect
 
 import (
 	"strconv"
-	"strings"
 )
 
 func NewMappingGBase() (mapping *SqlMapping) {
@@ -21,19 +20,6 @@ func NewMappingGBase() (mapping *SqlMapping) {
 	mapping.PackPageSql = func(selectSql string, pageSize int, pageNo int) (pageSql string) {
 		pageSql = `SELECT * FROM(SELECT ROWNUM rn,t.* FROM(` + selectSql + `) t WHERE ROWNUM <=` + strconv.Itoa(pageSize*pageNo) + ")"
 		pageSql += `WHERE rn>=` + strconv.Itoa(pageSize*(pageNo-1)+1)
-		return
-	}
-	mapping.ReplaceSqlVariable = func(sqlInfo string, args []interface{}) (variableSql string) {
-		strList := strings.Split(sqlInfo, "?")
-		if len(strList) < 1 {
-			variableSql = sqlInfo
-			return
-		}
-		variableSql = strList[0]
-		for i := 1; i < len(strList); i++ {
-			variableSql += ":" + strconv.Itoa(i)
-			variableSql += strList[i]
-		}
 		return
 	}
 
